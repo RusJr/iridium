147c2b094a3eed33b2946a0750bbebb58749434e
RusJr <r.davletbayev@kazdream.kz>
2018-06-19 18:07:23 +0600

    0.1.51 - Added file logging, headless mode

--- iridium/__init__.py
+++ iridium/__init__.py
@@ -1,19 +1,1 @@
-import logging.config
-
 from iridium.browser import ChromeBrowser
-
-
-# LOGGING TODO: hmm ----------------------------------------------------------------------------------------------------
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {'ir_format': {'format': '[%(levelname)s] [%(asctime)s] [%(name)s] %(message)s',
-                                 'datefmt': '%d/%m/%Y %H:%M:%S'}},
-    'handlers': {
-        'ir_handler': {'level': 'DEBUG', 'class': 'logging.StreamHandler', 'formatter': 'ir_format'},
-    },
-    'loggers': {
-        'Iridium': {'handlers': ['ir_handler'], 'propagate': False, 'level': 'DEBUG'},
-    }
-}
-logging.config.dictConfig(LOGGING)
--- iridium/browser.py
+++ iridium/browser.py
@@ -1,10 +1,13 @@
 import logging.config
 import os
 import platform
+from logging import FileHandler
+from logging.handlers import RotatingFileHandler
 from time import sleep
 from typing import List
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 from iridium.actions import BrowserAction
 
@@ -25,10 +28,11 @@ class ChromeBrowser:
     window_size = (1366, 768)
     page_load_timeout = 15
 
-    def __init__(self):
-        self.browser = None
+    def __init__(self, logging_file=None, headless=False):
         self.logger = logging.getLogger('Iridium')
-        self._open_browser()
+        self._init_logging(logging_file)
+        self.browser = None
+        self._open_browser(headless)
 
     def __del__(self):
         self._close_browser()
@@ -52,14 +56,38 @@ class ChromeBrowser:
         except KeyError:
             raise Exception('OS detecting problem (%s)' % platform_name)
 
-    def _open_browser(self):
-        self.browser = webdriver.Chrome(self._chromedriver_path)
+    def _open_browser(self, headless=False):
+
+        options = Options()
+        options.headless = headless
+
+        self.browser = webdriver.Chrome(self._chromedriver_path, chrome_options=options)
         self.browser.set_window_size(*self.window_size)
-        # self.browser.delete_all_cookies()
+        self.browser.delete_all_cookies()
         self.browser.set_page_load_timeout(self.page_load_timeout)
         self.logger.debug('Started browser instance')
 
     def _close_browser(self):
-        if self.browser:
+        if hasattr(self, 'browser') and self.browser:
             self.browser.quit()
-        self.logger.debug('browser closed')
+        # self.logger.debug('browser closed')
+
+    def _init_logging(self, logging_file=None):
+        log_dict = {
+            'version': 1,
+            'disable_existing_loggers': False,
+            'formatters': {'ir_format': {'format': '[%(levelname)s] [%(asctime)s] [%(name)s] %(message)s',
+                                         'datefmt': '%d/%m/%Y %H:%M:%S'}},
+            'handlers': {'ir_console': {'level': 'DEBUG', 'class': 'logging.StreamHandler', 'formatter': 'ir_format'}},
+            'loggers': {'Iridium': {'handlers': ['ir_console'], 'propagate': False, 'level': 'DEBUG'}, }
+        }
+
+        if logging_file:
+            log_dict['handlers']['ir_file'] = {'class': 'logging.handlers.RotatingFileHandler',
+                                               'filename': logging_file,
+                                               'mode': 'w',
+                                               'formatter': 'ir_format', }
+                                               # 'maxBytes': 10485760}
+            log_dict['loggers']['Iridium']['handlers'].append('ir_file')
+
+        logging.config.dictConfig(log_dict)
